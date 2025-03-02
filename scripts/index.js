2ce4b008115d6f86e706fb75c53b2d80617414de
AyeshaSareen-Dev <121003581+AyeshaSareen-Dev@users.noreply.github.com>
2025-03-02 21:36:21 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 const discoverNewDiv = document.getElementById('discover-new-div');
 
 discoverNewDiv.addEventListener('click', function(){
-    window.location.href = "/";
+    window.location.href = "../blog.html";
 });
 
 const buttons = document.querySelectorAll('.completed-btn');
